a62e4a9f4d88bebdaec44e365804ae85550d0b8c
Felipe Tavares <felipe.amtavares1@gmail.com>
2022-06-01 23:39:38 -0300

    Unit Test Model

--- tests/Unit/App/ModelsUserTest.php
+++ tests/Unit/App/ModelsUserTest.php
@@ -16,7 +16,7 @@ class ModelsUserTest extends TestCase
         return new User();
     }
 
-    public function test_traits()
+    public function test_has_traits()
     {
         $traits = array_keys(class_uses($this->model()));
 
@@ -27,5 +27,36 @@ class ModelsUserTest extends TestCase
         ];
 
         $this->assertEquals($expectedTraits, $traits);
+    }
+
+    public function test_has_fillable()
+    {
+        $fillable = $this->model()->getFillable();
+
+        $expectedFillable = [
+            'name',
+            'email',
+            'password',
+        ];
+
+        $this->assertEquals($expectedFillable, $fillable);
+    }
+
+    public function test_incrementing_is_false()
+    {
+        $this->assertFalse($this->model()->incrementing);
+    }
+
+    public function test_has_casts()
+    {
+        $expectedCasts = [
+            'id' => 'string',
+            'email_verified_at' => 'datetime',
+        ];
+
+        $casts = $this->model()->getCasts();
+        dump($casts);
+
+        $this->assertEquals($expectedCasts, $casts);
     }
 }
