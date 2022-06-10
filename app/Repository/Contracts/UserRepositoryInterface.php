439222e03f2942c0ff46e8a541de3f17df9271e2
Felipe Tavares <felipe.amtavares1@gmail.com>
2022-06-10 19:17:46 -0300

    ntegration Test Repository - Delete and Exception

--- app/Repository/Contracts/UserRepositoryInterface.php
+++ app/Repository/Contracts/UserRepositoryInterface.php
@@ -7,4 +7,5 @@ interface UserRepositoryInterface
     public function getAll(): array;
     public function create(array $data): object;
     public function update(string $email, array $data): object;
+    public function delete(string $email): bool;
 }
