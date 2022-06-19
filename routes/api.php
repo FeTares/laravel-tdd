11b2f5fa4089f8b48f7d49603cd2501eeb089cc6
Felipe Tavares <felipe.amtavares1@gmail.com>
2022-06-18 21:39:58 -0300

    Tests E2E api users

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\{
+    UserController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', function () {
-    return response()->json([]);
-});
+Route::get('/users', [UserController::class, 'index']);
